198abe47468a8428f8c1296bb883e7a786e19eda
Kiefer Lam <kieferlam@gmail.com>
2020-07-23 16:50:44 +0100

    Build 23072020

---
+++ build/precache-manifest.fd755c656e56a34ae5177b81aba69868.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e9d1aca4ee1c33eb48589fa0cc7336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "543f2896bffd2c323749",
+    "url": "/static/css/main.6a28bc6a.chunk.css"
+  },
+  {
+    "revision": "3973bdf584cdd0569c67",
+    "url": "/static/js/2.fdd32d73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fdd32d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543f2896bffd2c323749",
+    "url": "/static/js/main.51ec7d61.chunk.js"
+  },
+  {
+    "revision": "5736845322f5c59f6308",
+    "url": "/static/js/runtime-main.f8790ff7.js"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "e0c2cbf22a653c3e5b1988dd921d0044",
+    "url": "/static/media/SourceSerifPro-Regular.e0c2cbf2.ttf"
+  },
+  {
+    "revision": "ec3bd298b01747783ab747d37f039046",
+    "url": "/static/media/portrait.ec3bd298.jpg"
+  }
+]);
